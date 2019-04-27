bb4549fc55d984c3e91be4d769153f758e0b2e67
Roopa Madihally <roopabdr@gmail.com>
2019-04-27 23:23:00 +0530

    Updates

---
+++ precache-manifest.36050667e31bc5156c65df56c3d1bbf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73fbcfe054ca11da52cbad9c2e85073",
+    "url": "/monthly-todo/index.html"
+  },
+  {
+    "revision": "efa8e591f6424247fb51",
+    "url": "/monthly-todo/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/monthly-todo/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "efa8e591f6424247fb51",
+    "url": "/monthly-todo/static/js/main.b30bf247.chunk.js"
+  },
+  {
+    "revision": "8b1593bb374a3be6f3bb",
+    "url": "/monthly-todo/static/js/runtime~main.9ecd28e3.js"
+  }
+]);
