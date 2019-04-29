f295b43dc5af83715488f781e395184266a01707
Roopa Madihally <roopabdr@gmail.com>
2019-04-29 23:50:38 +0530

    Updates

---
+++ precache-manifest.b9d7eccf21ea320f45eeaef6d5a7abbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8cbcfb520525f746af4944d39dfee3",
+    "url": "/monthly-todo/index.html"
+  },
+  {
+    "revision": "2cbd1f686f6897b610c3",
+    "url": "/monthly-todo/static/css/main.545eaaa1.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/monthly-todo/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "2cbd1f686f6897b610c3",
+    "url": "/monthly-todo/static/js/main.ff3c0d7b.chunk.js"
+  },
+  {
+    "revision": "8b1593bb374a3be6f3bb",
+    "url": "/monthly-todo/static/js/runtime~main.9ecd28e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/monthly-todo/precache-manifest.6bd74cec733edc10af1bc32e5cfe5ad2.js"
+  "/monthly-todo/precache-manifest.b9d7eccf21ea320f45eeaef6d5a7abbf.js"
 );
 
 self.addEventListener('message', (event) => {
