c9515e67357fb3f9aa887ef903113a5a4d1a0100
Roopa Madihally <roopabdr@gmail.com>
2019-06-23 17:06:29 +0530

    added local storage functionality, added code to clear input field on user input

--- src/App.js
+++ src/App.js
@@ -4,22 +4,40 @@ import './App.css';
 import ItemList from './ItemList/ItemList';
 
 class App extends Component {
-  state = {
-    itemName: '',
-    items: [],
-  };
+  
+    constructor(props) {
+        super(props);
+        if (!(localStorage.getItem('items'))) {
+            let itemsArray = [];
+            localStorage.setItem('items', JSON.stringify(itemsArray));
+        }
+        else {
+            this.state = {
+                itemName: '',
+                items: JSON.parse(localStorage.getItem('items')),
+            };
+        }
+    }
+
+  saveToLocalStorage = (label, value) => {
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem(label, JSON.stringify(value));
+    }
+  }
 
   onAddClick = () => {
     let listOfItems = [...this.state.items];
     listOfItems.push({ name: this.state.itemName, strikeThrough: false });
-    this.setState({items: listOfItems});
+    this.setState({itemName: '', items: listOfItems});
+    this.saveToLocalStorage('items', listOfItems);
   }
 
   onItemListKeyPress = () => {
-    console.log('onItemListKeyPress');
+    // console.log('onItemListKeyPress');
     let listOfItems = [...this.state.items];
     listOfItems.push({ name: this.state.itemName, strikeThrough: false });
-    this.setState({items: listOfItems});
+    this.setState({itemName: '', items: listOfItems});
+    this.saveToLocalStorage('items', listOfItems);
   }
 
   onInputChange = (itemName) => {
@@ -27,17 +45,19 @@ class App extends Component {
   }
 
   onItemListClick = (element, id) => {
-    console.log(element, 'clicked', id);
+    // console.log(element, 'clicked', id);
     if ( element.includes('trash') ) {
       let listOfItems = [...this.state.items];
       let index = id;
       listOfItems.splice(index, 1);
       this.setState({ items: listOfItems });
+      this.saveToLocalStorage('items', listOfItems);
     } else {
         let listOfItems = [...this.state.items];
         let index = id;
         listOfItems[index].strikeThrough = !listOfItems[index].strikeThrough;
         this.setState({ items: listOfItems });
+        this.saveToLocalStorage('items', listOfItems);
     }
   }
 
@@ -48,6 +68,7 @@ class App extends Component {
           add={ this.onAddClick } 
           change={ this.onInputChange }
           keyPress={ this.onItemListKeyPress }
+          value = { this.state.itemName }
           />
         <div className='item-wrapper'>
         {
--- src/ItemInput/ItemInput.js
+++ src/ItemInput/ItemInput.js
@@ -9,7 +9,8 @@ const ItemInput = (props) => {
                 className='input' 
                 placeholder='Add Item to List..' 
                 onChange={ (event) => props.change(event.target.value) } 
-                onKeyPress={ (event) => event.key === 'Enter'? props.keyPress() : console.log('god knows') }
+                onKeyPress={ (event) => event.key === 'Enter'? props.keyPress() : null }
+                value = { props.value }
             />
             <button className='button' onClick={ props.add }>Add</button>
         </div>
